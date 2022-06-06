c9b7882e23430c2c0e44a98b160f2171bdef72d7
Victor Hernandez <victor-2027@hotmail.com>
2022-06-05 21:49:27 -0500

    chore:

--- src/pages/DocumentManagement/Rols/Rols.tsx
+++ src/pages/DocumentManagement/Rols/Rols.tsx
@@ -2,11 +2,23 @@ import HeaderPlus from "../../../ui/Header/HeaderPlus";
 import styled from "styled-components";
 import RolsTable from "./RolsTable";
 import Container from "../../../ui/Container";
+import { useState } from "react";
 
 export const Rols = () => {
+  const [visibleModal, setVisibleModal] = useState(false);
+//   const [changeData, setChangeData] = useState(false);
+
+  const onToggleModal = () => {
+    setVisibleModal(!visibleModal);
+  };
+
+//   const onUpdateTable = () => {
+//     setChangeData(true);
+//   };
+
   return (
     <StyledContainer width="100%">
-      <HeaderPlus title="ROLES" />
+      <HeaderPlus title="ROLES" setVisibleModal={onToggleModal} />
       <RolsTable />
     </StyledContainer>
   );
