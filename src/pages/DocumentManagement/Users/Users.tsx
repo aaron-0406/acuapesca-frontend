a19e591a99842bf6275671b687a13d856f3b2dbe
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-06-11 23:48:31 -0500

    chore: fix users

--- src/pages/DocumentManagement/Users/Users.tsx
+++ src/pages/DocumentManagement/Users/Users.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { StyledContainer } from "../../../ui/Container/Container.styled";
+import styled from "styled-components";
+import Container from "../../../ui/Container";
 import HeaderPlus from "../../../ui/Header/HeaderPlus";
 import UsersProvider from "./UsersContext";
 import UsersModalCreate from "./UsersModal/UsersModalCreate";
@@ -38,3 +39,7 @@ export const Users = () => {
     </UsersProvider>
   );
 };
+
+const StyledContainer = styled(Container)`
+  height: 100vh;
+`;
